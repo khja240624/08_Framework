a281e836bf709c7c81daaaa8a0156c232e2d40f4
khja240624 <khja240624@gmail.com>
2024-09-26 17:24:46 +0900

    20240926_테스트 편의성 기능, 파일 업로드

--- boardProject/src/main/java/edu/kh/project/main/mapper/MainMapper.java
+++ boardProject/src/main/java/edu/kh/project/main/mapper/MainMapper.java
@@ -3,6 +3,7 @@ package edu.kh.project.main.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import edu.kh.project.member.dto.Member;
 
@@ -14,4 +15,26 @@ public interface MainMapper {
 	 */
 	List<Member> selectMemberList();
 
+	/** 빠른 로그인
+	 * @param memberNo
+	 * @return loginMember
+	 */
+	Member directLogin(int memberNo);
+
+	/** 비밀번호 초기화
+	 * @param memberNo
+	 * @param encPw
+	 * @return result
+	 */
+	int resetPw(@Param("no") int memberNo, @Param("pw") String encPw);
+
+	/** 회원 탈퇴 상태 변경
+	 * @param memberNo
+	 * @return result
+	 */
+	int changeStatus(int memberNo);
+	
+
+	
+	
 }
